08c4af2a70ea65f4054d115ae8a625e141d25acb
cat <catforgor@proton.me>
2026-06-26 09:23:01 +0100

    feat: hook_api for named api also did some refactoring for readability

--- include/minhook.hpp
+++ include/minhook.hpp
@@ -32,7 +32,7 @@ class Hook {
   Hook(void* target, fn_ptr detour)
       : target_(target),
         status_(MH_CreateHook(target, reinterpret_cast<void*>(detour),
-                              reinterpret_cast<void**>(&original_))) {}
+                              &original_)) {}
 
   Hook(fn_ptr target, fn_ptr detour)
       : Hook(reinterpret_cast<void*>(target), detour) {}
@@ -56,12 +56,30 @@ class Hook {
 
   [[nodiscard]] MH_STATUS status() const { return status_; }
 
-  fn_ptr original() const { return original_; }
+  fn_ptr original() const { return reinterpret_cast<fn_ptr>(original_); }
 
  private:
+  Hook(MH_STATUS status) : target_(nullptr), status_(status) {}
+
+  template <typename F>
+  friend Hook<F> hook_api(const wchar_t*, const char*,
+                          typename Hook<F>::fn_ptr);
+
   void* target_ = nullptr;
-  fn_ptr original_ = nullptr;
+  void* original_ = nullptr;
   MH_STATUS status_ = MH_UNKNOWN;
 };
+
+template <typename Fn>
+  requires std::is_function_v<Fn>
+Hook<Fn> hook_api(const wchar_t* module, const char* proc,
+                  typename Hook<Fn>::fn_ptr detour) {
+  if (auto hmod = GetModuleHandleW(module); !hmod)
+    return {MH_ERROR_MODULE_NOT_FOUND};
+  else if (auto addr = GetProcAddress(hmod, proc); !addr)
+    return {MH_ERROR_FUNCTION_NOT_FOUND};
+  else
+    return {reinterpret_cast<void*>(addr), detour};
+}
 
 }  // namespace mh
